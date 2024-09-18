ee178a362f3208cac31bef295a9c76ceec9a05a3
roberto furlan <furlanrgf@gmail.com>
2024-09-18 17:58:00 -0300

    add backend folder

---
+++ src/domains/suporte/infra/database/repository.ts
@@ -0,0 +1,29 @@
+import * as mongoDB from "mongodb";
+
+export class Repository {
+    url: string;
+    database!: string
+    collection!: string
+
+    constructor(url: string) {
+        this.url = url
+    }
+
+    protected async getCollection(database: string, collection: string): Promise<mongoDB.Collection> {
+        this.database = database
+        this.collection = collection
+
+        const client: mongoDB.MongoClient = new mongoDB.MongoClient(this.url)
+        
+        await client.connect();
+
+        const db: mongoDB.Db = client.db(database);
+  
+        return db.collection(collection);     
+    }
+
+    protected toObjectId(id: string): mongoDB.ObjectId {
+        return new mongoDB.ObjectId(id);
+    }
+
+}
